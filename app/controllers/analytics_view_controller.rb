df0b53f59b23c991947d730ced6a26d38adb02e7
Luke <email@luke-roberts.info>
2012-09-22 15:45:55 +0100

    added some model and view templates for later use

--- app/controllers/analytics_view_controller.rb
+++ app/controllers/analytics_view_controller.rb
@@ -2,6 +2,7 @@ class AnalyticsViewController < UIViewController
 
   def initWithNibName(name, bundle: bundle)
     super
+    self.view.backgroundColor = UIColor.underPageBackgroundColor
     self.tabBarItem = UITabBarItem.alloc.initWithTitle("Analytics", image:nil, tag:2)
     self
   end
---
+++ app/views/graph_view.rb
@@ -0,0 +1,5 @@
+class GraphView < UIView
+  :attr_accessor :tweets
+  def animate
+  end
+end
