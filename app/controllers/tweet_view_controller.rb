9244d1c4c15dbbf2725469310604addb2a96de2a
Jon Doveston <jon@doveston.me.uk>
2012-09-22 16:05:18 +0100

    formatting

--- app/controllers/tweet_view_controller.rb
+++ app/controllers/tweet_view_controller.rb
@@ -1,5 +1,4 @@
 class TweetViewController < UIViewController
-
   def initWithParams(params = {})
     @id = params[:id]
     @search_engine = SearchEngine.initWithDelegate(self)
@@ -9,14 +8,14 @@ class TweetViewController < UIViewController
 
   def viewDidLoad
     super
-    self.title = "Display Tweet"
+    self.title = 'Display Tweet'
     self.view.backgroundColor = UIColor.underPageBackgroundColor
     @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
     @button.setFrame([[30,200],[200,50]])
-    unless Array(App::Persistence['tweets']).include? @id
-      @button.setTitle("Add To Favouries", forState:UIControlStateNormal)
+    unless Array(App::Persistence['tweets']).include?(@id)
+      @button.setTitle('Add To Favouries', forState:UIControlStateNormal)
     else
-      @button.setTitle("Remove From Favouries", forState:UIControlStateNormal)
+      @button.setTitle('Remove From Favouries', forState:UIControlStateNormal)
     end
     @button.addTarget(self, action: :add_or_remove_from_favourites, forControlEvents:UIControlEventTouchUpInside)
   end
@@ -34,15 +33,14 @@ class TweetViewController < UIViewController
     tweets = App::Persistence['tweets']
     if tweets.nil?
       tweets = [@id]
-      @button.setTitle("Remove from Favouries", forState:UIControlStateNormal)
+      @button.setTitle('Remove from Favouries', forState:UIControlStateNormal)
     elsif !tweets.include? @id
       tweets = tweets + [@id]
-      @button.setTitle("Remove from Favouries", forState:UIControlStateNormal)
+      @button.setTitle('Remove from Favouries', forState:UIControlStateNormal)
     else
       tweets = tweets - [@id]
-      @button.setTitle("Add To Favouries", forState:UIControlStateNormal)
+      @button.setTitle('Add To Favouries', forState:UIControlStateNormal)
     end
     App::Persistence['tweets'] = tweets
   end
-
 end
